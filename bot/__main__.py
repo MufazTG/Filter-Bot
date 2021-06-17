6a33151a295951c73f531235ae0fb2e04a3ef249
Mufaz TG <85679056+MufazTG@users.noreply.github.com>
2021-06-17 10:11:56 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -172,8 +172,9 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="⚙️Help",callback_data="help_back"),InlineKeyboardButton(text="Master😴",url="https://t.me/D_ar_k_Angel")]]
-    keyboard += [[InlineKeyboardButton(text="♻️Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="Add Me➕",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="🔊 Update Channel 🔊",url="t.me/bx_botz),InlineKeyboardButton(text="Master 😇",url="https://t.me/Mufaz123")]]
+    keyboard += [[InlineKeyboardButton(text="♻️ Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚙️Help",url=",callback_data="help_back")]
+    keyboard += [[InlineKeyboardButton(text="Add Me To Group➕",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
